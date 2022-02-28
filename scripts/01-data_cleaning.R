a4b1f7eaed0685d72f8a6f0c95f1d7f990493337
jenniferliu888 <99062932+jenniferliu888@users.noreply.github.com>
2022-02-28 00:46:44 -0500

    Add files via upload

---
+++ scripts/01-data_cleaning.R
@@ -0,0 +1,15 @@
+#### Preamble ####
+# Purpose: Data Cleaning 
+# Author: Jennifer Liu, Zhiyi Liu, Jiayi Wu
+# Data: 28 February 2022
+# Contact: jenliu.liu@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - 
+# Any other information needed?
+
+# We used processed data, please download 
+# from https://www.openicpsr.org/openicpsr/project/144981/version/V1/view 
+
+
+           
